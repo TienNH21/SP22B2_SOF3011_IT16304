6d84affa59b4ae908f1023e59bc9f861423f81a6
HoangTien339 <tiennh21@fpt.edu.vn>
2022-03-15 12:06:04 +0700

    JSP/EL - Layout - CRUD Servlet

---
+++ src/main/java/controllers/admin/UserServlet.java
@@ -0,0 +1,61 @@
+package controllers.admin;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({
+	"/users/index",
+	"/users/show",
+	"/users/create",
+	"/users/store",
+	"/users/edit",
+	"/users/update",
+	"/users/delete",
+})
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public UserServlet() {
+        super();
+    }
+
+	protected void doGet(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		if (uri.contains("index")) {
+			this.index(request, response);
+		} else if (uri.contains("show")) {
+			this.show(request, response);
+		} else if (uri.contains("create")) {
+//			this.create(request, response);
+		} else if (uri.contains("edit")) {
+//			this.edit(request, response);
+		} else if (uri.contains("delete")) {
+//			this.delete(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+	
+	private void index(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		//
+	}
+	
+	private void show(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		//
+	}
+
+}
