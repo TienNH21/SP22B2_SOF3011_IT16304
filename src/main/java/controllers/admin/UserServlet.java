349ba77f4f5bccc6c432fbc683e840ac154d04c2
HoangTien339 <tiennh21@fpt.edu.vn>
2022-03-17 12:04:56 +0700

    Lecture 5: JSTL

--- src/main/java/controllers/admin/UserServlet.java
+++ src/main/java/controllers/admin/UserServlet.java
@@ -1,11 +1,17 @@
 package controllers.admin;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import beans.form_data.RegisterData;
 
 @WebServlet({
 	"/users/index",
@@ -33,25 +39,91 @@ public class UserServlet extends HttpServlet {
 		} else if (uri.contains("show")) {
 			this.show(request, response);
 		} else if (uri.contains("create")) {
-//			this.create(request, response);
+			this.create(request, response);
 		} else if (uri.contains("edit")) {
-//			this.edit(request, response);
+			this.edit(request, response);
 		} else if (uri.contains("delete")) {
-//			this.delete(request, response);
+			this.delete(request, response);
+		} else {
+			// 404
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		if (uri.contains("store")) {
+			this.store(request, response);
+		} else if (uri.contains("update")) {
+			this.update(request, response);
+		} else {
+			// 404
+		}
 	}
 	
 	private void index(
 		HttpServletRequest request,
 		HttpServletResponse response
 	) throws ServletException, IOException {
-		//
+		Date now = new Date();
+		RegisterData o1 = new RegisterData("Ng Van A", "HN",
+			"0123", "anv@gmail.com", "123123", 1, 0),
+			o2 = new RegisterData("Ng Van A", "HN", "0123",
+				"anv@gmail.com", "123123", 1, 0),
+			o3 = new RegisterData("Ng Van A", "HN", "0123",
+				"anv@gmail.com", "123123", 1, 0);
+		
+		List<RegisterData> ds = new ArrayList<RegisterData>();
+		ds.add(o1);
+		ds.add(o2);
+		ds.add(o3);
+		
+		request.setAttribute("ds", ds);
+		request.setAttribute("now", now);
+		request.setAttribute("view",
+			"/views/admin/users/index.jsp");
+		request.getRequestDispatcher("/views/layout.jsp")
+		.forward(request, response);
 	}
 	
 	private void show(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		//
+	}
+	
+	private void create(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		//
+	}
+	
+	private void edit(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		//
+	}
+	
+	private void delete(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		//
+	}
+	
+	private void update(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		//
+	}
+	
+	private void store(
 		HttpServletRequest request,
 		HttpServletResponse response
 	) throws ServletException, IOException {
