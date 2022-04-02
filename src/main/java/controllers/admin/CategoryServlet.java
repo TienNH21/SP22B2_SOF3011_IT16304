bf68d846e5cce75418a628f1316d603f005df2b9
HoangTien339 <tiennh21@fpt.edu.vn>
2022-04-02 10:32:36 +0700

    Relation Mapping

---
+++ src/main/java/controllers/admin/CategoryServlet.java
@@ -0,0 +1,55 @@
+package controllers.admin;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDAO;
+import entities.Category;
+import entities.User;
+
+@WebServlet("/categories")
+public class CategoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAO userDAO;
+
+    public CategoryServlet() {
+        super();
+        this.userDAO = new UserDAO();
+    }
+
+	protected void doGet(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		List<User> dsUser = this.userDAO.all();
+		request.setAttribute("dsUser", dsUser);
+		request.getRequestDispatcher(
+			"/views/admin/categories/create.jsp"
+		).forward(request, response);
+	}
+
+	protected void doPost(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) throws ServletException, IOException {
+		Category cate = new Category();
+		String ten = request.getParameter("ten");
+		
+		int id = Integer.parseInt(
+			request.getParameter("user_id")
+		);
+		
+		User user = this.userDAO.findById(id);
+		
+		cate.setTen(ten);
+		cate.setUser(user);
+		
+		// TODO: this.categoryDAO.create(cate);
+	}
+}
